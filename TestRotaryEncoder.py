afb49f5177174642b45c943ad621182fff6fa2fa
William Stark <williamstarkgithub@gmail.com>
2021-08-10 17:26:58 +1000

    Created TestRotaryEncoder.py
    Tests the rotary encoder by running the motor and reporting step values and calculates speeds

--- TestRotaryEncoder.py
+++ TestRotaryEncoder.py
@@ -1,14 +1,15 @@
-# Example code taken from the ECE3091 moodle page
 import gpiozero
 import time
 
 # You can replace the pin with whichever pin you are using to drive the PWM input to the motor driver
 pwm = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
+encoder = gpiozero.RotaryEncoder(a=5, b=6,max_steps=100000)
 
+pre_steps = 0
 for j in range(10):
     pwm.value = j/10
     direction.value = not direction.value
     print('Duty cycle:',pwm.value,'Direction:',direction.value)
     time.sleep(5.0)
-
-pwm.off()
+    print('Counter:',encoder.steps,'Speed:',(encoder.steps-pre_steps)/5.0,'steps per second\n')
+    pre_steps = encoder.steps
