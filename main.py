8a8f6ca181d28b3c7bfa3b5abc3105eeb6649cd7
William Stark <wsta0003@student.monash.edu>
2021-08-28 20:25:25 +1000

    pipe changes

--- main.py
+++ main.py
@@ -21,21 +21,24 @@ encoder_r = gpiozero.RotaryEncoder(a=23, b=24,max_steps=100000)  # using GPIO 23
 robot = Robot(3, 10, motor_l, motor_r, encoder_l, encoder_r)
 #sensor = DistanceSensor(echo=18,trigger=17) # echo and trigger on pins 18 and 17
 
-def DriveToGoal(x, y,q, kill_pipe):
+# Thread for driving to a goal location
+def DriveToGoal(x, y,pipe, kill_pipe):
     v_desired = 5 # move at 5cm per second
     w_desired = np.pi/3 # rotate a half turn in 3 seconds
     count = 0
-    robot.q=q
+    robot.pipe=pipe
     robot.kill_pipe = kill_pipe
     robot.drive_to_point(x, y, v_desired, w_desired)
     print("end")
     #q.put("DriveToGoal Finished")
 
+# Thread for just testing an extra thread doing stuff
 def Test():
     for i in range(3):
         time.sleep(1)
         print("test thread")
 
+# Thread for checking the ultrasonic sensor and reporting collisions
 def CheckUltrasonicSensor(q):
     dt = 0.01 # check every hundredth of a second for a collision
     time_started = time.time()
@@ -49,15 +52,16 @@ def CheckUltrasonicSensor(q):
             break
         time.sleep(dt)
 
-
+## Main thread here
 if __name__ == '__main__':
-    q=Queue()
     x=5 # at 30cm away from origin in x-direction
     y=5# at 30cm away from origin in y-direction
     dtg_kill_pipe_PARENT, dtg_kill_pipe_CHILD = Pipe()
-    drive_to_goal_process = Process(target=DriveToGoal, args=(x,y,q,dtg_kill_pipe_CHILD))
+    dtg_pipe_PARENT, dtg_pipe_CHILD = Pipe()
+    drive_to_goal_process = Process(target=DriveToGoal, args=(x,y,dtg_pipe_CHILD,dtg_kill_pipe_CHILD))
     test_other = Process(target=Test)
-    sensor_process = Process(target=CheckUltrasonicSensor, args=(q,))
+    sensor_pipe_PARENT, sensor_pipe_CHILD = Pipe()
+    sensor_process = Process(target=CheckUltrasonicSensor, args=(sensor_pipe_CHILD,))
     # put all processes into this array so the thread manager (main thread) can keep track of them
     processes = [drive_to_goal_process, test_other, sensor_process]
     print("hi")
