ab84c3b155df600f7367fbb5ea720c8c1439e823
William Stark <wsta0003@student.monash.edu>
2021-10-05 08:20:01 +1100

    Some changes

--- TestServoControlServo.py
+++ TestServoControlServo.py
@@ -4,12 +4,12 @@ from gpiozero.pins.mock import MockFactory, MockPWMPin
 import numpy as np
 from gpiozero import Servo
 
-servo = Servo(17) # Insert the pin number of the servo output here
+servo = Servo(21) # Insert the pin number of the servo output here
 
 while True:
     servo.min()
     time.sleep(1)
-    servo.mid()
+    servo.value = 0.1
     time.sleep(1)
     servo.max()
     time.sleep(1)
